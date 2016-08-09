9b3ae7e7b01bbe58665bd3fa45e81488e6be526c
Cesar Valiente <cesar.valiente@gmail.com>
2016-08-09 17:00:56 +0200

    Lesson 15. Kotlin Android Extensions

--- app/src/main/java/com/cesarvaliente/kotlinforecastapp/ui/ForecastListAdapter.kt
+++ app/src/main/java/com/cesarvaliente/kotlinforecastapp/ui/ForecastListAdapter.kt
@@ -4,14 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.cesarvaliente.kotlinforecastapp.R
 import com.cesarvaliente.kotlinforecastapp.domain.model.Forecast
 import com.cesarvaliente.kotlinforecastapp.domain.model.ForecastList
 import com.cesarvaliente.kotlinforecastapp.ui.utils.ctx
 import com.squareup.picasso.Picasso
-import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.item_forecast.view.*
 
 class ForecastListAdapter(private val weekForecast: ForecastList, private val itemClick: (Forecast) -> Unit) :
         RecyclerView.Adapter<ForecastListAdapter.Viewholder>() {
@@ -31,27 +29,14 @@ class ForecastListAdapter(private val weekForecast: ForecastList, private val it
 
     class Viewholder(view: View, private val itemClick: (Forecast) -> Unit) :
             RecyclerView.ViewHolder(view) {
-        private val iconView: ImageView
-        private val dateView: TextView
-        private val descriptionView: TextView
-        private val maxTemperatureView: TextView
-        private val minTemperatureView: TextView
-
-        init {
-            iconView = view.find(R.id.icon)
-            dateView = view.find(R.id.date)
-            descriptionView = view.find(R.id.description)
-            maxTemperatureView = view.find(R.id.maxTemperature)
-            minTemperatureView = view.find(R.id.minTemperature)
-        }
 
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
-                Picasso.with(itemView.ctx).load(iconUrl).into(iconView)
-                dateView.text = date
-                descriptionView.text = description
-                maxTemperatureView.text = "${high.toString()} C"
-                minTemperatureView.text = "${low.toString()} C"
+                Picasso.with(itemView.ctx).load(iconUrl).into(itemView.icon)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = "${high.toString()} C"
+                itemView.minTemperature.text = "${low.toString()} C"
                 itemView.setOnClickListener { itemClick(this) }
             }
         }
--- app/src/main/java/com/cesarvaliente/kotlinforecastapp/ui/MainActivity.kt
+++ app/src/main/java/com/cesarvaliente/kotlinforecastapp/ui/MainActivity.kt
@@ -3,11 +3,10 @@ package com.cesarvaliente.kotlinforecastapp.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.cesarvaliente.kotlinforecastapp.R
 import com.cesarvaliente.kotlinforecastapp.domain.commands.RequestForecastCommand
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
@@ -25,15 +24,16 @@ class MainActivity : AppCompatActivity() {
         //val forecastList = findViewById(R.id.forecast_list) as RecyclerView
 
         //Using Anko. Creates and extension function of the Activity (check source code of "find")
-        val forecastList: RecyclerView = find(R.id.forecast_list)
+//        val forecastList: RecyclerView = find(R.id.forecast_list)
 
-        forecastList.layoutManager = LinearLayoutManager(this)
+        //Using Kotlin Android Extensions we just use the name given by its id in the xml (forecastList)
+        foreCastList.layoutManager = LinearLayoutManager(this)
 
         doAsync() {
             val result = RequestForecastCommand("94043").execute()
             uiThread {
                 val adapter = ForecastListAdapter(result) { toast(it.date) }
-                forecastList.adapter = adapter
+                foreCastList.adapter = adapter
             }
         }
     }
